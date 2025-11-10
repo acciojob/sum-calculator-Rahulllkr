9d63dcc1e0a03479dfdeaa05b46cf02ccdf68f93
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-10 22:28:38 +0530

    now done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [] = useState(0)
+  const [num,setNum] = useState(0)
   const [sum,setSum] = useState(0)
  useEffect(() => {
   setSum(sum + num)
